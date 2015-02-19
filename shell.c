fd7fac07043600082e9c815757a3a4efced6fe71
Declan Neilson <declangoldneilson@hotmail.com>
2015-02-19 23:09:09 +0000

    Added very basic tokenizer
    It's a bit hacky and you should all feel free to butcher it up but it
    works and doesn't crash with all inputs i think. Defo needs a bit of
    work. Thought about turning commands into enums to make finding a
    command easier than using strcomp but that's up to you guys.

--- shell.c
+++ shell.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_CMD_LEN 512
+#define MAX_CMD_SIZE 10
 
-char* getCommand() {
+char** getCommand() {
 	static char str[MAX_CMD_LEN];
+	static char* output[MAX_CMD_SIZE];
+	
+	memset(output, 0, MAX_CMD_SIZE); // Reset array, since it's static it'd normally retain values between commands
 	printf("> ");
 	
 	// Get input
 	// fgets returns null if ctrl-D is pressed, so return exit if so
 	if (fgets(str, sizeof(str), stdin) == NULL){
-		return "exit";
+		strcpy(output[0], "exit");
 	}
 
 	// If last character in string is not \n, input was too long, so extra 
@@ -23,14 +28,35 @@ char* getCommand() {
 	// Remove newline
 	str[strlen(str) - 1] = '\0';
 	
-	return str;
+	// Iterate through the string using strtok to split into whitespace seperated tokens
+	// If there's a better way to do this feel free to tear it up
+	// Also malloc some stuff onto the first entry 
+	
+	int i = 0;
+	char* input = strtok(str, " ");
+	output[0] = malloc(MAX_CMD_LEN); // bit hacky, to stop the while loop not finding something at index 0 and crashing spectacularly
+	strcpy(output[0], "");
+	
+	while(input){
+		output[i] = malloc(MAX_CMD_LEN); // Allocating space to a new array entry
+		strcpy(output[i], input); // Copying the next input into the array
+		input = strtok(NULL, " "); // Getting next input
+		i++;
+	};
+	
+	return output;
 }
 
 int main(int argc, char *argv[]) {
-	char* command;
+	char** tokens; // Array of string tokens, command is first entry
 	
-	while(strcmp((command = getCommand()), "exit") != 0) {
-		printf("Command entered: %s\n", command);
+	
+	while(strcmp((tokens = getCommand())[0], "exit") != 0) {
+		int n = 0;
+		while(tokens[n] != NULL){
+			printf("Command entered: %s\n", tokens[n]); // Just a test to see if it's working okay
+			n++;
+		}
 	}
 	
 	printf("Quitting...\n");
